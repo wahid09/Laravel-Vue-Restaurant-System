fbc6535a1293fadfa020cb3bc8eeb3a8e6385061
wahid09 <wahid.ullah.cse@gmail.com>
2019-11-27 20:02:50 +0600

    Menu migration and factory and controller create

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Menu::class, function (Faker $faker) {
+	$name = $faker->word;
+    $slug = str_slug($name);
+    return [
+        'name' => $name,
+        'slug' => $slug,
+        'description' => $faker->paragraph($nbSentences = 2, $variableNbSentences = true),
+        'price' => $faker->numberBetween($min = 99, $max = 999),
+        'image' => $faker->imageUrl($width = 400, $height = 480),
+    ];
+});
